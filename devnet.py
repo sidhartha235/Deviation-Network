ad40c81339e9ea7bb419e421faa2aec77bcf9c29
sidhartha235 <ananthulasidhartha03@gmail.com>
2024-08-20 12:08:43 +0530

    modify few deprecated features

--- devnet.py
+++ devnet.py
@@ -12,8 +12,8 @@ August4–8, 2019, Anchorage, AK, USA.ACM, New York, NY, USA, 10 pages. https://
 import numpy as np
 np.random.seed(42)
 import tensorflow as tf
-tf.set_random_seed(42)
-sess = tf.Session()
+tf.random.set_seed(42)
+sess = tf.compat.v1.Session()
 
 from keras import regularizers
 from keras import backend as K
